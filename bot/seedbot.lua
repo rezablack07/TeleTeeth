154089814b406915ad1297188a69bf21b61951e3
rezablack077 <oxford_sajadi@hotmail.com>
2017-03-02 00:06:32 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = { 256331224,0,tonumber(our_id)},--Sudo users
+    sudo_users = { 256331224,193068389,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
